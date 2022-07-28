28a447d4e42a78325e9b273cef226f1f467bd397
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-07-28 16:48:28 -0400

    lesson-14

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     class="flex flex-col min-h-screen font-Roboto bg-weather-primary"
   >
     <SiteNavigation />
-    <RouterView />
+    <RouterView class="flex-1" v-slot="{ Component }">
+      <Transition name="page">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -12,4 +16,12 @@ import { RouterView } from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from {
+  opacity: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,28 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: "Home",
+      },
     },
     {
       path: "/weather/:state/:city",
       name: "cityView",
       component: CityView,
+      meta: {
+        title: "Weather",
+      },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state
+      ? `${to.params.city}, ${to.params.state}`
+      : to.meta.title
+  } | The Local Weather`;
+  next();
 });
 
 export default router;
