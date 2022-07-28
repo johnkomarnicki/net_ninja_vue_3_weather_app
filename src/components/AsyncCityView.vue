c2b38bcf563178c12699d814d0ed27b179716e08
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-07-28 16:20:29 -0400

    lesson-8

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+    );
+
+    // cal current date & time
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime =
+      utc + 1000 * weatherData.data.timezone_offset;
+
+    // cal hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime =
+        utc + 1000 * weatherData.data.timezone_offset;
+    });
+
+    return weatherData.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const weatherData = await getWeatherData();
+
+const router = useRouter();
+const removeCity = () => {
+  const cities = JSON.parse(localStorage.getItem("savedCities"));
+  const updatedCities = cities.filter(
+    (city) => city.id !== route.query.id
+  );
+  localStorage.setItem(
+    "savedCities",
+    JSON.stringify(updatedCities)
+  );
+  router.push({
+    name: "home",
+  });
+};
+</script>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <Suspense>
+      <AsyncCityView />
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import AsyncCityView from "../components/AsyncCityView.vue";
+</script>
