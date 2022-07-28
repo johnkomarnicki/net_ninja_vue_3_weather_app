d662deeefc48a33f57845cf0d01bd20dc1bd9477
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-07-28 16:09:05 -0400

    lesson-5
    Search & Display Cities With Mapbox Geocoding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,59 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+          @click="previewCity(searchResult)"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey =
+  "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
+
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
+
+<style lang="scss" scoped></style>
