5d93e2d38fee78596ebeaef4ea8242569ead735a
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-07-28 16:35:23 -0400

    lesson-11

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -46,7 +46,6 @@ const goToCityView = (city) => {
     name: "cityView",
     params: { state: city.state, city: city.city },
     query: {
-      id: city.id,
       lat: city.coords.lat,
       lng: city.coords.lng,
     },
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -95,7 +95,6 @@ const addCity = () => {
 
   let query = Object.assign({}, route.query);
   delete query.preview;
-  query.id = locationObj.id;
   router.replace({ query });
 };
 
