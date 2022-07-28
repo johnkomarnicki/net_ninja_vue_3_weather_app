53b4259db26c424d438f5cb360c3c8e4814a6d74
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-07-28 16:27:43 -0400

    lesson-11

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,6 +33,14 @@
         </template>
       </ul>
     </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+        <template #fallback>
+          <p>Loading...</p>
+        </template>
+      </Suspense>
+    </div>
   </main>
 </template>
 
@@ -40,6 +48,7 @@
 import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
+import CityList from "../components/CityList.vue";
 
 const router = useRouter();
 const previewCity = (searchResult) => {
