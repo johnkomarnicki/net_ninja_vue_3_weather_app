4de22cfd4d9b79ebc3ab7f62a3208cd8122c76a3
Ivan Zhdanov <tain.krios@gmail.com>
2022-08-26 16:20:06 +0200

    v-if for remove button

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex flex-col flex-1 items-center">
     <!-- Banner -->
-    <div
-      v-if="route.query.preview"
-      class="text-white p-4 bg-weather-secondary w-full text-center"
-    >
+    <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
       <p>
         You are currently previewing this city, click the "+"
         icon to start tracking this city.
@@ -15,22 +12,22 @@
       <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
       <p class="text-sm mb-12">
         {{
-          new Date(weatherData.currentTime).toLocaleDateString(
-            "en-us",
-            {
-              weekday: "short",
-              day: "2-digit",
-              month: "long",
-            }
-          )
+        new Date(weatherData.currentTime).toLocaleDateString(
+        "en-us",
+        {
+        weekday: "short",
+        day: "2-digit",
+        month: "long",
+        }
+        )
         }}
         {{
-          new Date(weatherData.currentTime).toLocaleTimeString(
-            "en-us",
-            {
-              timeStyle: "short",
-            }
-          )
+        new Date(weatherData.currentTime).toLocaleTimeString(
+        "en-us",
+        {
+        timeStyle: "short",
+        }
+        )
         }}
       </p>
       <p class="text-8xl mb-8">
@@ -43,13 +40,9 @@
       <p class="capitalize">
         {{ weatherData.current.weather[0].description }}
       </p>
-      <img
-        class="w-[150px] h-auto"
-        :src="
+      <img class="w-[150px] h-auto" :src="
           `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
-        "
-        alt=""
-      />
+        " alt="" />
     </div>
 
     <hr class="border-white border-opacity-10 border w-full" />
@@ -59,27 +52,19 @@
       <div class="mx-8 text-white">
         <h2 class="mb-4">Hourly Weather</h2>
         <div class="flex gap-10 overflow-x-scroll">
-          <div
-            v-for="hourData in weatherData.hourly"
-            :key="hourData.dt"
-            class="flex flex-col gap-4 items-center"
-          >
+          <div v-for="hourData in weatherData.hourly" :key="hourData.dt" class="flex flex-col gap-4 items-center">
             <p class="whitespace-nowrap text-md">
               {{
-                new Date(
-                  hourData.currentTime
-                ).toLocaleTimeString("en-us", {
-                  hour: "numeric",
-                })
+              new Date(
+              hourData.currentTime
+              ).toLocaleTimeString("en-us", {
+              hour: "numeric",
+              })
               }}
             </p>
-            <img
-              class="w-auto h-[50px] object-cover"
-              :src="
+            <img class="w-auto h-[50px] object-cover" :src="
                 `http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`
-              "
-              alt=""
-            />
+              " alt="" />
             <p class="text-xl">
               {{ Math.round(hourData.temp) }}&deg;
             </p>
@@ -94,28 +79,20 @@
     <div class="max-w-screen-md w-full py-12">
       <div class="mx-8 text-white">
         <h2 class="mb-4">7 Day Forecast</h2>
-        <div
-          v-for="day in weatherData.daily"
-          :key="day.dt"
-          class="flex items-center"
-        >
+        <div v-for="day in weatherData.daily" :key="day.dt" class="flex items-center">
           <p class="flex-1">
             {{
-              new Date(day.dt * 1000).toLocaleDateString(
-                "en-us",
-                {
-                  weekday: "long",
-                }
-              )
+            new Date(day.dt * 1000).toLocaleDateString(
+            "en-us",
+            {
+            weekday: "long",
+            }
+            )
             }}
           </p>
-          <img
-            class="w-[50px] h-[50px] object-cover"
-            :src="
+          <img class="w-[50px] h-[50px] object-cover" :src="
               `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
-            "
-            alt=""
-          />
+            " alt="" />
           <div class="flex gap-2 flex-1 justify-end">
             <p>H: {{ Math.round(day.temp.max) }}</p>
             <p>L: {{ Math.round(day.temp.min) }}</p>
@@ -124,10 +101,10 @@
       </div>
     </div>
 
-    <div
+    <div 
+      v-if="route.query.id"
       class="flex items-center gap-2 py-12 text-white cursor-pointer duration-150 hover:text-red-500"
-      @click="removeCity"
-    >
+      @click="removeCity">
       <i class="fa-solid fa-trash"></i>
       <p>Remove City</p>
     </div>
